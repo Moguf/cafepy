50fcc2bb0acbc5e19f7a92043545a969a4f5070c
Moguf <kbu94985@gmail.com>
2016-07-20 20:57:32 +0900

    add a main in cafepy

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# cafepy
-== Python3 scripts ==
+# cafepy(Python3 scripts)
 These scripts are for analyzing CafeMol outputs.
+
+
+Example:
+```bash
+cafepy dist -i filename.dcd -o output.file
+
+```
--- src/cafepy.py
+++ src/cafepy.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
+# coding: utf-8
 
+import argparse
+
+class CafePy(object):
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+    def args(self):
+        pass
+
+
+if __name__ == "__main__":
+    tmp = CafePy()
+    tmp.main()
+    
 
