87b530b1ba79705d862c54cb4b93fda4bf185bd7
Mogu programming <kbu94984@gmail.com>
2016-08-05 14:55:13 +0900

    before merge3:test_read_dcd.py

--- test/test_read_dcd.py
+++ test/test_read_dcd.py
@@ -20,7 +20,6 @@ class TestReadDcd(unittest.TestCase):
     def test_readHeader(self):
         self.testclass.readHeader()
         
-
     def test_get_onestep_wiht_index(self):
         self.testclass.readHeader()
         self.assertEqual(initial_cordinates_of_test_data,self.testclass[0])
@@ -28,20 +27,25 @@ class TestReadDcd(unittest.TestCase):
         self.assertEqual(final_cordinates_of_test_data,self.testclass[2999])
         ## check final_cordinate
 
-
     def test_get_length_of_trajectory_with_len(self):
-        print(len(self.testclass))
-
+        self.testclass.readHeader()
+        self.assertEqual(3001,len(self.testclass))
+        self.testclass._header.tstep = 3
+        self.assertEqual(3001,len(self.testclass))
+        
     def test_get_a_trajectory_in_loop(self):
-        for i in self.testclass:
-            print(i)
+        self.testclass.readHeader()
+        for i,xyz in enumerate(self.testclass[1:10]):
+            self.assertEqual(nine_trajectories[i],xyz)
         
     def test_get_trajectory_with_slieces(self):
-        print(self.testclass[2:4])
-        print(self.testclass[2::2])
-        print(self.testclass[2::2])
-        print(self.testclass[-2:-4])
+        self.testclass.readHeader()
+        self.assertEqual(nine_trajectories,self.testclass[1:10])
+        #print(self.testclass[2::2])
+        #print(self.testclass[2::2])
+        #print(self.testclass[-2:-4])
 
+        
     def test_readDCD(self):
         pass
     
