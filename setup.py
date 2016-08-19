ddc4800f791949c25722f65fe936a5996fc64c6a
Moguf <kbu94984@gmail.com>
2016-08-19 23:07:33 +0900

    MOD:setup.py

--- setup.py
+++ setup.py
@@ -4,3 +4,11 @@
 from distutils.core import setup
 
 
+setup(name='CafePy',
+      version='0.1',
+      description='Python Scripts For Analizing CafeMol Data.',
+      author='Mogu',
+      author_email='kbu94984@gmail.com',
+      url='',
+      packages=['src'],
+    )
