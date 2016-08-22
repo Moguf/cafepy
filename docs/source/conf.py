cfe3fd638a07e676933466c15d611ebeb41bd19e
Moguf <kbu94984@gmail.com>
2016-08-22 16:40:49 +0900

    MOD: path in conf.py.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('/home/ono/misc/scripts/cafepy'))
+sys.path.insert(0, os.path.abspath('../../../cafepy/cafepy'))
 
 # -- General configuration ------------------------------------------------
 
